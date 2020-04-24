d91e33bd1ccb7ba4ec90c607cc90a2f92e20bc33
diploduquitos <xavicoll6@gmail.com>
2020-04-24 13:35:21 +0200

    Amb Cjt_clusters i Cluster

---
+++ Cluster.hh
@@ -0,0 +1,83 @@
+/** @file Cluster.hh
+ * 	@author Xavier Coll Ribas
+ * 	@brief Especificació de la classe <em>Cluster</em>
+ */
+#ifndef CLUSTER_HH
+#define CLUSTER_HH
+#ifndef NO_DIAGRAM
+#include <iostream>
+#include <map>
+#include <string>
+#include "BinTree.hh"
+#endif
+#include "Cjt_especies.hh"
+using namespace std;
+
+
+/** @brief <em>Crea un cluster</em> */
+class Cluster {
+
+private:
+
+	BinTree< pair<string, double> >cluster; // 
+	
+	void distancia_entre_clusters(const Cluster& clus1, const Cluster& clus2);
+		
+public:
+		// Constructores
+/**
+ * 	@brief Crea un cluster buit. 
+ * 
+ *	@brief <b>Precondició: </b>Cert.
+ * 
+	@brief <b>Postcondició: </b>El resultat és un cluster buit.
+*/	
+	Cluster();
+	
+/**
+ * 	@brief Crea un cluster a partir d'una espècie. 
+ * 
+ *	@brief <b>Precondició: </b>Cert.
+ * 
+	@brief <b>Postcondició: </b>El resultat és un cluster de la especie demanada.
+*/	
+	
+	Cluster(const Especie& esp);
+/**
+ * 	@brief Crea un cluster a partir de la fusió de dos altres. 
+ * 
+ *	@brief <b>Precondició: </b>Els dos clusters existeixen i no són buits.
+ * 
+	@brief <b>Postcondició: </b>El resultat és un cluster amb la fusió dels dos altres.
+*/	
+	
+	Cluster(Cluster clus1, Cluster Clus2);
+	
+		// Consultores
+
+/**
+ * 	@brief Consulta el subarbre del cluster, amb els fills dret i esquerra. 
+ * 
+ *	@brief <b>Precondició: </b>Cert.
+ * 
+	@brief <b>Postcondició: </b>S'ha consultat el subarbre.
+*/	
+	void sub_arbre_cluster(const Cluster& clus1);
+	
+		// Lectura i Escriptura
+		
+/**
+ * 	@brief Escriu un cluster. 
+ * 
+ *	@brief <b>Precondició: </b>Cert.
+ * 
+	@brief <b>Postcondició: </b>S'ha escrit el cluster.
+*/	
+		
+	void escriure();
+	
+
+};
+
+
+#endif
